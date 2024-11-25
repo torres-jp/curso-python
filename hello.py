8af183178a7eeb84d03c14c2bc69eb5bebedab58
pnauj <torres.jpw@gmail.com>
2024-11-24 21:18:23 -0300

    starting python course

---
+++ hello.py
@@ -0,0 +1,18 @@
+# print('hello world') 
+
+
+# Esto es un comentario
+# print('Esto sale en consola') # Este es un comentario al costado del codigo
+
+
+"""
+ Este comentario es mas largo
+ if 5 > 3:
+    # Este comentario dentro de la estructura if
+    if 5 > 4:
+        print('5 es mayor que 4 y que 3')
+        #otro comentario
+
+
+"""
+
---
+++ variable.py
@@ -0,0 +1,93 @@
+# Variables
+# Es un contenedor para almacenar valor de datos
+# Este contenedor  va a tener un nombre
+# Es creada la primera vez que se le asigna un valor
+
+
+# x = 5
+# X = 6 # caseSensitive 
+# txt = "Esto es texto"
+
+# print(x)
+# print(X)
+# print(txt)
+
+### nombres admitidos de variables
+
+# Puede empezar con una letra o guion bajo (underScore)
+
+
+
+# mivariable = "texto"
+# _mivariable = "texto 3"
+# miOtravariable = """
+# otro texto mas
+# """
+
+# _miOtravariablemas = '''
+# otro texto mas con comilla simples
+# '''
+
+# print(mivariable)
+# print(_mivariable)
+# print(miOtravariable)
+# print(_miOtravariablemas)
+
+
+### Una variable no puede iniciar con un numero
+# 5variable = "texto"
+# pring(5variable)
+
+
+### Solo pueden contener caracteres alfanumericos o guines bajos _
+# miVariable123 = "texto"
+# _miVariable123 = "texto 2"
+
+# ### CaseSensitive ( no solo al comienzo sino en general)
+# miVariable = "texto"
+# MiVariable = "texto"
+
+# print (miVariable123)
+# print (_miVariable123)
+# print (miVariable)
+# print (MiVariable)
+
+### Convenciones de escritura de variables
+# # camelCase
+# camelCase = "Comienza con minuscula y la palabra siguiente comienza en Mayuscula"
+
+# # PascalCase
+# PascalCase = "Comienza con Mayuscula y la palabra siguiente comienza en Mayuscula"
+
+# # Snake Case 
+# snake_case = "Comienza con minuscula y las palabras se unen con guion bajo"
+
+# print(camelCase)
+# print(PascalCase)
+# print(snake_case)
+
+
+##########################################
+
+# # Multi-Asignacion
+# x,y,z = 5,7,9
+# print(x)
+# print(y)
+# print(z)
+
+# a = b = c = 'Texto'
+
+# print(a)
+# print(b)
+# print(c)
+
+# b = "Diferente texto"
+
+# print(b)
+
+# frutas = ['banana', 'naranja', 'frutilla']
+# m,n,o = frutas
+
+# print(m)
+# print(n)
+# print(o)
