c5e076e76f69051f7f8918ffdac236d908196f08
pnauj <torres.jpw@gmail.com>
2024-12-04 13:51:12 -0300

    add list

--- bases/listas.py
+++ bases/listas.py
@@ -12,7 +12,7 @@ tipo = type(listaBool)
 listaDesdeConstructor = list(('Beta', 1, True))
 # print(listaDesdeConstructor)
 
-## Acceder a los datos
+## ACCEDER A LOS DATOS
 parteLista = frutas[1:3]
 desdeComienzo = frutas[:2] # Desde el comienzo hasta el elemento 2 incluido
 hastaFinal = frutas[2:] # Desde el elemento 2 hasta el final
@@ -20,7 +20,55 @@ hastaFinal = frutas[2:] # Desde el elemento 2 hasta el final
 
 ## verificar si existe algun elemento
 
-if 'anana' in frutas:
-    print("Si esta incluido")
-else:
-    print("No esta en la lista")
+# if 'anana' in frutas:
+#     print("Si esta incluido")
+# else:
+#     print("No esta en la lista")
+
+
+lenguajes = ['python', 'Java', 'c++', 'javaScript','ruby']
+
+lenguajes[3] = 'React'
+lenguajes[2:4] = ['numPy', 'Scrappy']
+lenguajes.insert(2,'c++') # inserta un dato en el lugar asignado
+lenguajes.append('ReactPy') # agrega un elemento a la lista
+
+
+frontEnd = ['React', 'Angular', 'Vue', 'Nextjs']
+lenguajes.extend(frontEnd)
+
+# lenguajes.remove('Java')
+# lenguajes.pop() # elimina el ultimo dato de la lista.
+
+# del lenguajes[5]
+# lenguajes.clear() # vacia la lista.
+
+
+## RECORRER LISTAS
+
+# for lenguaje in lenguajes:
+#     print(lenguaje)
+
+
+# for i in range(len(lenguajes)):
+#     print(f"{i+1}. {lenguajes[i]}")
+
+
+# [print(lenguaje) for lenguaje in lenguajes] # shorthand para una impresion de lista iterable
+
+# listaconY = []
+
+# for lenguaje in lenguajes:
+#     if 'y' in lenguaje:
+#         listaconY.append(lenguaje)
+
+# print(listaconY)
+
+
+# listaconY2 = [lenguaje.upper() for lenguaje in lenguajes if 'y' in lenguaje]
+# print(listaconY2)
+
+
+## ORDENAR UNA LISTA
+lenguajes.sort()
+print(lenguajes)
